ecb6bac0fbafa95547a54bfd927a4044dd2a4bae
Michael B <macowner@Macs-MacBook-Pro.local>
2016-09-30 16:51:35 -0700

    added notification to service

--- app/src/main/java/com/example/macowner/flickr_api/service/PollService.java
+++ app/src/main/java/com/example/macowner/flickr_api/service/PollService.java
@@ -2,13 +2,19 @@ package com.example.macowner.flickr_api.service;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
+import com.example.macowner.flickr_api.PhotoGalleryActivity;
+import com.example.macowner.flickr_api.R;
 import com.example.macowner.flickr_api.model.GalleryItem;
 import com.example.macowner.flickr_api.network.FlickrFetchr;
 import com.example.macowner.flickr_api.util.QueryPreferences;
@@ -79,6 +85,28 @@ public class PollService extends IntentService {
       Log.i(TAG, "Got an old result: " + resultId);
     } else {
       Log.i(TAG, "Got a new result: " + resultId);
+
+
+      Resources resources = getResources();
+      Intent i = PhotoGalleryActivity.newIntent(this);
+      PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+      Notification notification = new NotificationCompat.Builder(this)
+          //configure text
+          .setTicker(resources.getString(R.string.new_pictures_title))
+          //configure small icon, title and text
+          .setSmallIcon(android.R.drawable.ic_menu_report_image)
+          .setContentTitle(resources.getString(R.string.new_pictures_title))
+          .setContentText(resources.getString(R.string.new_pictures_text))
+          //specify what happens when user presses notification
+          .setContentIntent(pi)
+          .setAutoCancel(true)
+          .build();
+
+      //get an instance to post notification
+      NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+      notificationManagerCompat.notify(0, notification);
+
     }
 
     QueryPreferences.setLastResultId(this, resultId);
