8504251fc883bfc81fc41bdb7a14e654c8a66012
Michael B <macowner@Macs-MacBook-Pro.local>
2016-09-30 11:47:03 -0700

    added search query

---
+++ app/src/main/java/com/example/macowner/flickr_api/util/QueryPreferences.java
@@ -0,0 +1,26 @@
+package com.example.macowner.flickr_api.util;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by macowner on 9/30/16.
+ */
+public class QueryPreferences {
+
+  private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+  public static String getStoredQuery(Context context) {
+    return PreferenceManager.getDefaultSharedPreferences(context)
+        .getString(PREF_SEARCH_QUERY, null);
+  }
+
+  public static void setStoredQuery(Context context, String query) {
+    PreferenceManager.getDefaultSharedPreferences(context)
+        .edit()
+        .putString(PREF_SEARCH_QUERY, query)
+        .apply();
+  }
+
+
+}
